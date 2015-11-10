555f99dbd58be2060e76f40ef697d9cdc680dec6
Brian Woodward <brian.woodward@gmail.com>
2015-11-10 16:05:11 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * base-tree <https://github.com/doowb/base-tree>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
