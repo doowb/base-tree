5e08af77ef596ff40e0e85b41d37330170cdd6aa
Brian Woodward <brian.woodward@gmail.com>
2015-11-11 12:20:02 -0500

    refactoring to use `.name` on options and allow passing in custom `.tree` function on options.

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ var extend = require('extend-shallow');
 
 /**
  * Creates a `.tree` method on `app` that will recursively
- * generate a tree of nodes specified by the plural option passed in.
+ * generate a tree of nodes specified by the name option passed in.
  *
  * ```js
  * var app = new Base();
- * app.use(tree({plural: 'children'}));
+ * app.use(tree({name: 'children'}));
  *
  * app.children = {};
  * app.children.one = new Base();
@@ -27,8 +27,12 @@ var extend = require('extend-shallow');
  * ```
  *
  * @param  {Object} `options` Options to use in `.tree` method
+ * @param  {String} `options.name` Name of the collection object to look for child nodes.
+ * @param  {Function} `options.tree` Optional `tree` function to use to generate the node or tree of nodes for the current app. Takes `app` and `options` as parameters.
+ * @param  {Function} `options.getLabel` Get a label for the node being built. Takes `app` and `options` as parameters.
+ * @param  {Function} `options.getData` Get a data object for the node being built. Takes `app` and `options` as parameters.
  * @return {Function} plugin
- * @api true
+ * @api public
  * @name  tree
  */
 
@@ -45,33 +49,59 @@ module.exports = function(options) {
     // of nodes
     app.define('tree', function(opts) {
       opts = extend({}, options, opts);
-      var node = {
-        label: getLabel(app, opts),
-        data: getData(app, opts)
-      };
-
-      // get the plural name of the children to lookup
-      var plural = opts.plural || 'nodes';
-      var children = app[plural];
-      if (typeof children !== 'object') {
-        return node;
+      if (typeof opts.tree === 'function') {
+        return opts.tree.call(this, this, opts);
       }
-
-      // build a tree for each child node
-      node.nodes = [];
-      for (var key in children) {
-        var child = children[key];
-        if (typeof child.tree === 'function') {
-          node.nodes.push(child.tree(opts));
-        } else {
-          node.nodes.push(getLabel(child, opts));
-        }
-      }
-
-      return node;
+      return tree(this, opts);
     });
+
+    return fn;
   };
 };
+
+/**
+ * Default tree building function. Gets the label and data properties for the current `app` and
+ * recursively generates the child nodes and child trees if possible.
+ *
+ * This method may be overriden by passing a `.tree` function on options.
+ *
+ * @param  {Object} `app` Current application to build a node and tree from.
+ * @param  {Object} `options` Options used to control how the `label` and `data` properties are retreived.
+ * @return {Object} Generated node containing `label`, `data`, and `nodes` properties for current segment of a tree.
+ * @api public
+ * @name  options.tree
+ */
+
+function tree(app, options) {
+  var opts = extend({}, options);
+  var node = {
+    label: getLabel(app, opts),
+    data: getData(app, opts)
+  };
+
+  // get the name of the children to lookup
+  var name = opts.name || 'nodes';
+  var children = app[name];
+  if (typeof children !== 'object') {
+    return node;
+  }
+
+  // build a tree for each child node
+  var nodes = [];
+  for (var key in children) {
+    var child = children[key];
+    if (typeof child.tree === 'function') {
+      nodes.push(child.tree(opts));
+    } else {
+      nodes.push(tree(child, opts));
+    }
+  }
+
+  if (nodes.length) {
+    node.nodes = nodes;
+  }
+  return node;
+}
 
 /**
  * Figure out a label to add for a node in the tree.
@@ -80,15 +110,13 @@ module.exports = function(options) {
  * @param  {Object} `options` Pass `getLabel` on options to handle yourself.
  * @return {String} label to be shown
  * @api public
+ * @name  options.getLabel
  */
 
 function getLabel(app, options) {
   if (typeof options.getLabel === 'function') {
     return options.getLabel(app, options);
   }
-
-  if (typeof app === 'undefined') return;
-  if (typeof app === 'string') return app;
   return app.full_name || app.nickname || app.name;
 }
 
@@ -99,6 +127,7 @@ function getLabel(app, options) {
  * @param  {Object} `options` Pass `getData` on options to handle yourself.
  * @return {Object} data object to add to node
  * @api public
+ * @name  options.getData
  */
 
 function getData(app, options) {
